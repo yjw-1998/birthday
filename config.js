a4c3079af5a30a1287134ad74ed0d6f884258f05
orange <79147799+yjw-1998@users.noreply.github.com>
2022-12-26 19:14:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -37,6 +37,6 @@ var config = {
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "A MESSAGE FOR YOU ",
     }
 };
